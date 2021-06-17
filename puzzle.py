0d074afa0a6a28076ff3472562a422335bc17282
kalinosia <kalinowska953@gmail.com>
2021-06-17 21:07:16 +0200

    working, submitted, bot something missing...

--- puzzle.py
+++ puzzle.py
@@ -9,26 +9,50 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
+# P → Q = ¬P ∨ Q     Implication
+# P ⬌ Q = (P → Q) V (Q → P)      Biconditional
+# ¬(P ^ Q) = ¬P V ¬Q          De Morgan’s Law
+# (P ^ (Q V R)) = (P ^ Q) V (P ^ R)      Distributive Property
+# (P V (Q ^ R)) = (P V Q) ^ (P V R)
+
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    (AKnave and not(AKnight and AKnave)) or (AKnight and (AKnight and AKnave))
+    Or(AKnight, AKnave),
+    Implication(AKnight, (AKnight and AKnave)),
+    #Implication(AKnave, Not(AKnight and AKnave))
 )
-
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    (AKnave and not(AKnave and BKnave)) or (AKnight and (AKnave and BKnave))
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+    #Or(Not(AKnight), And(AKnave, BKnave)),
+    #Implication(A, B) = Not(A) or B
+    Implication(AKnight, And(AKnave, BKnave)),
+    #Or(Not(AKnave), Not(And(AKnave, BKnave)))
+    Implication(AKnave, Not(And(AKnave, BKnave)))
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # A is Knight and say true (both Knight or both knave OR A is Knave and say not true (both knight or both knave is not true)
-    And(AKnight and ((AKnight and BKnight) or (AKnave and BKnave))) or (AKnave and (not(AKnight and BKnight) or not(AKnave and BKnave))),
-    And(BKnight and ((AKnave and BKnight) or (AKnight and BKnave)) or (BKnave and (not(AKnave and BKnight) or not (AKnight and BKnave))))
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+    # A says "We are the same kind."
+    # A is Knight and say true (both Knight) OR A is Knave and say not true (both knave is not true) P → Q = ¬P ∨ Q
+    Implication(AKnight, And(AKnight, BKnight)),
+    Implication(AKnave, Not(And(AKnave, BKnave))),
+    # B says "We are of different kinds."
+    Implication(BKnight, (And(AKnave, BKnight))),
+    Implication(BKnave, Not(And(AKnight, BKnave)))
+    #Or(And(AKnight, Or(Not(BKnight), (Or(And(AKnight, BKnave), And(AKnave, BKnight))))),  And(AKnave, Or(Not(BKnave),  (Or(Not(And(AKnight, BKnave)), Not(And(AKnave, BKnight)))))))
 )
 
 # Puzzle 3
@@ -37,16 +61,40 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    (((AKnight and AKnight) or (AKnave and not AKnight)) or ((AKnight and AKnave ) or (AKnave and not AKnave))),
+    And(Or(AKnight, AKnave), Or(BKnight, BKnave), Or(CKnight, CKnave)),
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+    Not(And(CKnave, CKnight)),
 
-    ((BKnight and ((AKnight and AKnave) or (AKnave and not AKnave))) or (BKnave and not ((AKnight and AKnave) or (AKnave and not AKnave)))),
+    # A says either "I am a knight." or "I am a knave.", but you don't know which.
+    # #we need this???? probably not because this is has no info
 
-    ((BKnight and CKnave) or (BKnave and not CKnave)),
-
-    ((CKnight and AKnight) or (CKnave and not AKnight))
+    # B says "A said 'I am a knave'."
+    Implication(BKnight, Or(
+                Implication(AKnight, AKnave),
+                Implication(AKnave, Not(AKnave))
+    )),
+    Implication(BKnave, Not(Or(
+                Implication(AKnight, AKnave),
+                Implication(AKnave, Not(AKnave)))
+    )),
+    # B says "C is a knave."
+    Biconditional(BKnight, CKnave),
+    Biconditional(BKnave, Not(CKnave)),
+    # C says "A is a knight."
+    Biconditional(CKnight, AKnight),
+    Biconditional(CKnave, Not(AKnight))
 )
-
-
+'''
+letters=['A', 'B', 'C']
+for i in letters:
+    knowledge3.add(
+        Or(Symbol(f"{i}Knight"), Symbol(f"{i}Knave"))
+    )
+    knowledge3.add(
+        Not(And(Symbol(f"{i}Knight"), Symbol(f"{i}Knave")))
+    )
+'''
 def main():
     symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
     puzzles = [
